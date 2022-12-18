3b6071dd5472341ca6c02f46152b8490d5eb0c9f
Hammad Saeedi <faoqzuhair@gmail.com>
2022-12-19 00:21:59 +0500

    File I/O

--- src/FileOperations.java
+++ src/FileOperations.java
@@ -3,19 +3,21 @@ import java.io.*;
 public class FileOperations<T> {
 
     String path;
+    File file;
 
     public FileOperations(String path) {
         this.path = path + ".ser";
+        file = new File(this.path);
     }
 
     public void push(T object) throws IOException {
-        ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(new File(path)));
+        ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file));
         stream.writeObject(object);
         stream.close();
     }
 
     public T seek() throws ClassNotFoundException, IOException {
-        ObjectInputStream stream = new ObjectInputStream(new FileInputStream(new File(path)));
+        ObjectInputStream stream = new ObjectInputStream(new FileInputStream(file));
         @SuppressWarnings("unchecked")
         T obj = (T) stream.readObject();
         stream.close();
