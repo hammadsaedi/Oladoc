432089377ba6fb2fa3ec4cee5cb07e55cf4d29b6
Hammad Saeedi <faoqzuhair@gmail.com>
2023-01-09 06:32:01 +0500

    Time Setting Updates

--- src/Time.java
+++ src/Time.java
@@ -4,17 +4,11 @@ public class Time implements Serializable {
     private int second;
     private int minute;
     private int hour;
-    private int day;
-    private int month;
-    private int year;
 
     public Time(int second, int minute, int hour, int day, int month, int year) {
         this.second = second;
         this.minute = minute;
         this.hour = hour;
-        this.day = day;
-        this.month = month;
-        this.year = year;
     }
 
     public int getSecond() {
@@ -41,30 +35,6 @@ public class Time implements Serializable {
         this.hour = hour;
     }
 
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -80,48 +50,11 @@ public class Time implements Serializable {
             return false;
         if (hour != other.hour)
             return false;
-        if (day != other.day)
-            return false;
-        if (month != other.month)
-            return false;
-        if (year != other.year)
-            return false;
         return true;
-    }
-
-    private String month() {
-        switch (month) {
-            case 0:
-                return "January";
-            case 1:
-                return "February";
-            case 2:
-                return "March";
-            case 3:
-                return "April";
-            case 4:
-                return "May";
-            case 5:
-                return "June";
-            case 6:
-                return "July";
-            case 7:
-                return "August";
-            case 8:
-                return "September";
-            case 9:
-                return "October";
-            case 10:
-                return "November";
-            case 11:
-                return "December";
-            default:
-                return new String();
-        }
     }
 
     @Override
     public String toString() {
-        return hour + " : " + minute + " : " + second + " , " + day + " " + month() + " " + year;
+        return hour + " : " + minute + " : " + second;
     }
 }
